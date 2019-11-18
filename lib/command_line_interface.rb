60b05d391489a1d7ab5f30e288b644114c1638e4
Alice Freda <alicefreda@gmail.com>
2019-11-18 04:56:45 +0000

    Automatically backed up by Learn

--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -4,7 +4,7 @@ require 'nokogiri'
 require 'colorize'
 
 class CommandLineInterface
-  BASE_PATH = "https://learn-co-curriculum.github.io/student-scraper-test-page/"
+  BASE_PATH = "https://www.last.fm/user/thealice/library/artists?date_preset=LAST_365_DAYS"
 
   def run
     make_students
