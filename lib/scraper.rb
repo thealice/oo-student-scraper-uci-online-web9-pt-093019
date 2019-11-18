36e54e74d440986d005b69c3f469eb9219c2cf93
Alice Freda <alicefreda@gmail.com>
2019-11-18 04:53:56 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -48,6 +48,7 @@ class Scraper
 
   def self.scrape_last_fm_users_chart(username = thealice)
     user_chart = Nokogiri::HTML(open("https://www.last.fm/user/#{username}/library/artists?date_preset=LAST_365_DAYS"))
+    binding.pry
 
   end
 
